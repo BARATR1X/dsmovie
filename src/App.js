2f6220e840628851a09eb95461da98b7d7cda585
João Luis Vieira Escorcio <baratrix1@hotmail.com>
2022-05-11 19:27:06 -0300

    Projeto criado

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Olá DSMovie</h1>
+      <h3>Promoções</h3>
+      <h3>Agenda de filmes</h3>
+    </div>
+  );
+}
+
+export default App;
